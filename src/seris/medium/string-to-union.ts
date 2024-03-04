9985b3c0432af174c843fe77180a2143e0decdd7
broisnischal <neeswebservice@gmail.com>
2024-03-05 00:00:20 +0545

    challange-solve:string-to-union

--- src/seris/medium/string-to-union.ts
+++ src/seris/medium/string-to-union.ts
@@ -1,4 +1,5 @@
-type StringToUnion<T extends string> = T;
-
-type resultunion = StringToUnion<"test">;
+type StringToUnion<T extends string> = T extends `${infer Head}${infer Tail}`
+  ? Head | StringToUnion<Tail>
+  : never;
+type resultunion = StringToUnion<"any">;
 //    ^?
