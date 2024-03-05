03cb9693bf214c346a8eb208de93303563d36145
broisnischal <neeswebservice@gmail.com>
2024-03-05 23:40:55 +0545

    feat:changes

--- src/seris/medium/last-of-array.ts
+++ src/seris/medium/last-of-array.ts
@@ -1,7 +1,5 @@
+type Last1<T extends unknown[]> = T extends [...infer _, infer Tail]
+  ? Tail
+  : never;
 
-type Last1<T extends unknown[]> = T extends [...infer _, infer Tail] ? Tail : never;
-
-
-
-type Last<T extends unknown[]> =
-  [never, ...T][T["length"]]
+type Last<T extends unknown[]> = [never, ...T][T["length"]];
