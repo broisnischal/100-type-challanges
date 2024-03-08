dc36cf1430482886e1f3fc21fe37fce80b5338e7
broisnischal <neeswebservice@gmail.com>
2024-03-08 22:04:03 +0545

    typing

--- src/custom-challanges/urlparams.ts
+++ src/custom-challanges/urlparams.ts
@@ -1,7 +1,32 @@
 const url = "/cat/:id/:breed?size=sm";
 
-type urlType = typeof url;
+type URLType = typeof url;
 
-type URLc<T extends string> = T extends `:${infer V}` ? V : T;
+type ParamObject<T extends string> = T extends `:${infer P}${infer R}`
+  ? { [K in P]: string } & ParamObject<R>
+  : {};
 
-type result = URLc<urlType>;
+type PARAMResul = ParamObject<URLType>;
+
+type QueryObject<T extends string> = T extends `?${infer Q}`
+  ? Q extends `${infer K}=${infer V}&${infer Rest}`
+    ? { [K in K]: V } & QueryObject<Rest>
+    : Q extends `${infer K}=${infer V}`
+    ? { [K in K]: V }
+    : never
+  : {};
+
+type URLResult<T extends string> = {
+  params: ParamObject<T>;
+  query: QueryObject<T>;
+};
+
+type Result3 = URLResult<URLType>;
+
+// Test
+const result: Result3 = {
+  params: { id: "123", breed: "persian" },
+  query: { size: "sm" },
+};
+
+console.log(result);
